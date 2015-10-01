b79f959af671090e87fca5e2c9ff11261ea22521
nynhle <nynhle90@gmail.com>
2015-09-30 18:23:34 -1000

    User is now forced to enter a file path.

--- calculate_gc_content.py
+++ calculate_gc_content.py
@@ -4,37 +4,45 @@
 
 import sys
 
-a_count = 0.00
-c_count = 0.00
-g_count = 0.00
-t_count = 0.00
 
-with open(sys.argv[1], 'r') as fasta:
-    for line in fasta:
-        if line.startswith(">"):
-            # Header line, skip it
-            continue
-        a_count += line.count("A")
-        c_count += line.count("C")
-        g_count += line.count("G")
-        t_count += line.count("T")
+def calculations_and_print():
+    a_count = 0.00
+    c_count = 0.00
+    g_count = 0.00
+    t_count = 0.00
+    with open(sys.argv[1], 'r') as fasta:
+        for line in fasta:
+            if line.startswith(">"):
+                # Header line, skip it
+                continue
+            a_count += line.count("A")
+            c_count += line.count("C")
+            g_count += line.count("G")
+            t_count += line.count("T")
 
-print("Base counts for file %s:" % sys.argv[1])
-print("A: %d" % a_count)
-print("C: %d" % c_count)
-print("G: %d" % g_count)
-print("T: %d" % t_count)
+    print("Base counts for file %s:" % sys.argv[1])
+    print("A: %d" % a_count)
+    print("C: %d" % c_count)
+    print("G: %d" % g_count)
+    print("T: %d" % t_count)
 
-nucleotide_count = (a_count + g_count + c_count + t_count)
-GC_percentage = ((g_count + c_count)/nucleotide_count ) * 100
-A_percentage = (a_count/nucleotide_count) * 100
-C_percentage = (c_count/nucleotide_count) * 100
-G_percentage = (g_count/nucleotide_count) * 100
-T_percentage = (t_count/nucleotide_count) * 100
+    nucleotide_count = (a_count + g_count + c_count + t_count)
+    GC_percentage = ((g_count + c_count)/nucleotide_count ) * 100
+    A_percentage = (a_count/nucleotide_count) * 100
+    C_percentage = (c_count/nucleotide_count) * 100
+    G_percentage = (g_count/nucleotide_count) * 100
+    T_percentage = (t_count/nucleotide_count) * 100
 
-print "\n___Percentages___"
-print "GC: ",GC_percentage,"%" 
-print "A: ",A_percentage, "%"
-print "C ",C_percentage, "%"
-print "G: ",G_percentage, "%"
-print "T: ",T_percentage, "%"
+    print "\n___Percentages___"
+    print "GC: ",GC_percentage,"%" 
+    print "A: ",A_percentage, "%"
+    print "C ",C_percentage, "%"
+    print "G: ",G_percentage, "%"
+    print "T: ",T_percentage, "%"
+
+
+try:
+    calculations_and_print()
+except Exception:
+    print "Please provide a file path."
+
